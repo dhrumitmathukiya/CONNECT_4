f83e6c2de0f5ccabf4cc3a54f481d7fe23dc05d5
Prayag Gediya <prayaggediya60@gmail.com>
2022-06-23 15:35:06 +0530

    Initial Commit

---
+++ CONNECT 4/CONNECT_4App.swift
@@ -0,0 +1,17 @@
+//
+//  CONNECT_4App.swift
+//  CONNECT 4
+//
+//  Created by mac on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CONNECT_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CONNECT 4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CONNECT 4
+//
+//  Created by mac on 23/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
